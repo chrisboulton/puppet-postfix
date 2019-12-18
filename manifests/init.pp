590e04be8dec9576ca33aa3fd38f1d20c779bd30
Chris Boulton <chrisboulton@users.noreply.github.com>
2019-12-18 11:58:17 +1100

    use the built in augeas lens for managing transports instead of a custom one

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,14 +36,6 @@ class postfix(
     notify  => Exec['rebuild postfix transport'],
   }
 
-  # puppet in masterless mode won't synchronize augeas lenses, so do this manually
-  file { '/usr/share/augeas/lenses/postfix_transport.aug':
-    source => "puppet:///modules/${module_name}/augeas/postfix_transport.aug",
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-  }
-
   exec { 'rebuild postfix transport':
     command     => "postmap ${transport_file}",
     refreshonly => true,
